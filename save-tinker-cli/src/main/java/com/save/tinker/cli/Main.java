a01404bb08d9941afc0de303f7c3c15eba3e8d3c
novi.li <novi.li@temu.com>
2025-11-25 17:46:04 +0800

    feat: multy modules

--- save-tinker-cli/src/main/java/com/save/tinker/cli/Main.java
+++ save-tinker-cli/src/main/java/com/save/tinker/cli/Main.java
@@ -4,6 +4,6 @@ import com.save.tinker.core.HelloTest;
 
 public class Main {
     public static void main(String[] args) {
-
+        HelloTest.run();
     }
 }
--- save-tinker-core/src/main/java/com/save/tinker/core/HelloTest.java
+++ save-tinker-core/src/main/java/com/save/tinker/core/HelloTest.java
@@ -1,7 +1,7 @@
 package com.save.tinker.core;
 
 public class HelloTest {
-    public static void test() {
+    public static void run() {
         System.out.println("Multi-module OK!");
     }
 }
